b015c3bb36fefdba2fd3b1d726c19f4e4421c6ec
mweiss17 <martin.weiss@mail.mcgill.ca>
2015-11-20 14:36:23 -0500

    api routing

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 'use strict';
 
 // Declare variables used
-var express, require, scraper, bodyParser, app;
+var express, require, scraper, bodyParser, app, url;
 express = require('express');
 bodyParser = require('body-parser')
 app = express()
-
+url = require('url');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -20,6 +20,23 @@ app.use(express.static('scraper/images'));
 app.get('/', function (req, res) {
     res.render('index');
 });
+app.get('/zoom-levels', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({
+         '0': {width: 625,    height: 625},
+         '1': {width: 1250,   height: 1250},
+         '2': {width: 2500,   height: 2500},
+         '3': {width: 5000,   height: 5000},
+         '4': {width: 10000,  height: 10000}
+       }));
+});
+app.get('/tile', function(req, res){
+	var url_parts = url.parse(req.url, true);
+	var query = url_parts.query;
+	console.log(query.height);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(query));
+})
 
 app.listen(app.get('port'), function(req, res) {
  console.log('Server listening at ' + app.get('port'));
